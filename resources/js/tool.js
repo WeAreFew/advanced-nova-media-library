dac04800d60f5c5741e70d6fd1952ad9976a071b
Alex Chalupka <lupka@few.io>
2022-11-21 23:57:46 -0600

    moved around the import and its working now

--- resources/js/tool.js
+++ resources/js/tool.js
@@ -1,10 +1,12 @@
+import Tool from './components/Tool';
+
 Nova.booting((Vue, router) => {
     console.log('route added');
     router.addRoutes([
         {
             name: 'media-library',
             path: '/media-library',
-            component: require('./components/MediaLibrary'),
+            component: Tool,
         },
     ]);
 });
